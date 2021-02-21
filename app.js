ca3fefef01f95d0cc5cf01ebd597366e01eff3a8
USRF20R00C02 <dianadem1606@gmail.com>
2021-02-21 10:27:57 +0200

    node

--- app.js
+++ app.js
@@ -1,33 +1,113 @@
+const express = require('express');
+const expressHbs = require('express-handlebars');
 const fs = require('fs');
 const path = require('path');
+const userspath = path.join(__dirname, 'userslist', 'users.json');
+const app = express();
 
-let pathmale = path.join(__dirname, '1800');
-let pathfemale = path.join(__dirname, '2000');
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
-function change(path18, path20) {
-    fs.readdir(path18, (err2, files) => {
-        if (err2) {
-            console.log(err2);
-        } else {
-            files.forEach(data => {
-                fs.readFile(path.join(path18, data), (err, data) => {
-                    if (err) {
-                        console.log(err)
-                    }
-                    const objectJson = JSON.parse(data.toString());
-                    if (objectJson.gender === 'male') {
-                        fs.rename(path18, path20, err1 => {
-                            if (err1) {
-                                console.log(err1)
-                            }
-                        })
-                    }
-                })
-            })
+app.use(express.static(path.join(__dirname, 'static')));
+
+app.set('view engine', '.hbs');
+app.engine('.hbs', expressHbs({
+    defaultLayout: false
+}));
+app.set('views', path.join(__dirname, 'static'));
+
+
+app.get('/users', (req, res) => {
+
+    fs.readFile(userspath, ((err, data) => {
+        if (err) {
+            res.render('error');
+            return
         }
-    })
-}
 
-change(pathmale, pathfemale)
+        const users = JSON.parse(data.toString());
+        res.render('users',{users});
+        console.log(users);
+    }))
+})
 
-change(pathfemale, pathmale)
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+
+app.post('/login', (req, res) => {
+    const {password, mail} = req.body;
+
+    console.log(req.body);
+    fs.readFile(userspath, ((err1, data) => {
+        if (err1) {
+            res.render('error');
+            return
+        }
+        const users = JSON.parse(data.toString());
+        const findUser = users.find(user => user.mail === mail && user.password === password);
+        if (!findUser) {
+            res.redirect('/error');
+            return
+        }
+        res.redirect('/users');
+        console.log(users)
+    }))
+});
+
+
+app.get('/registration', (req, res) => {
+    res.render('registration');
+});
+app.get('/error', (req, res) => {
+    res.render('error');
+})
+
+app.post('/registration', (req, res) => {
+    const {mail} = req.body;
+
+    console.log(req.body);
+    fs.readFile(userspath, ((err2, data) => {
+        if (err2) {
+            res.render('error');
+            return
+        }
+
+        const users = JSON.parse(data.toString());
+        const findUser = users.find(user => user.mail === mail);
+
+        if (!findUser) {
+            users.push(req.body);
+            fs.writeFile(userspath, JSON.stringify(users), (err3) => {
+                if (err3) {
+                    res.render('error');
+                }
+            });
+            res.redirect('/users');
+            return
+
+        }
+        res.redirect('/error');
+    }))
+});
+
+app.get('/users/:userId', (req, res) => {
+    const {userId} = req.params;
+
+    console.log(req.params);
+    fs.readFile(userspath, (err4, data) => {
+        if (err4) {
+            console.log(err4);
+            return;
+        }
+    const users = JSON.parse(data.toString());
+    res.render('user', {user: users[userId]});
+})});
+
+
+app.listen(5000, () => {
+    console.log('App listen 5000');
+})
+
